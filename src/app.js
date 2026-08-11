143082d446331141d6532383e79b26b0d29ec86d
Perpaulo <perpaulo.varca@cvsu.edu.ph>
2026-08-11 13:33:08 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
 import routes from "./routes/index.js";
 
 dotenv.config();
@@ -9,14 +8,16 @@ dotenv.config();
 const app = express();
 
 app.use(
-    cors({
-        origin: "http://localhost:5174",
-        credentials: true
-    })
+  cors({
+    origin: [
+      "http://localhost:5174",
+      "https://cybence-scheduler-system-ojt-projec.vercel.app"
+    ],
+    credentials: true
+  })
 );
 
 app.use(express.json());
-
 app.use("/api", routes);
 
 export default app;
